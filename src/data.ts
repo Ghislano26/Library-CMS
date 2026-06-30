34f9c053ef5f596e9c0e668e6e1caf537b671280
Ghislano26 <ghislainatebachouacha@gmail.com>
2026-06-30 16:57:57 +0100

    verification deploys

--- src/data.ts
+++ src/data.ts
@@ -3,89 +3,217 @@ import { Book } from './types';
 export const INITIAL_BOOKS: Book[] = [
   {
     id: 'le-petit-prince',
-    title: 'Le Petit Prince',
-    author: 'Antoine de Saint-Exupéry',
-    genre: 'Conte Philosophique',
-    description: 'Une œuvre poétique et philosophique sous l\'apparence d\'un conte pour enfants, interrogeant l\'amour, l\'amitié et le sens de la vie.',
+    title: 'LE CHEMIN DE LA REUSITE',
+    author: 'Alain Foaleng',
+    genre: 'Roman Philosophique',
+    description: `La ville de Yaoundé s’éveillait lentement sous les premiers rayons du soleil. Dans le quartier
+                  Nkaobang, les rues de terre rouge retrouvaient leur agitation habituelle. Les mototaxis traversaient
+                  les ruelles à toute vitesse, les commerçantes installaient leurs étals, tandis que les enfants, cartable
+                  au dos, se dirigeaient vers leurs écoles.`,
     coverColor: 'from-blue-950 via-slate-900 to-indigo-950',
     coverDesignType: 'artistic',
     createdAt: '2026-01-15T08:00:00Z',
     tags: ['Classique', 'Philosophie', 'Poésie', 'Jeunesse'],
-    pagesCount: 96,
+    pagesCount: 12,
     chapters: [
       {
         id: 'lpp-ch1',
-        title: 'Chapitre I : Le Dessin de Boa',
-        content: `Lorsque j'avais six ans j'ai vu, une fois, une magnifique image, dans un livre sur la Forêt Vierge qui s'appelait "Histoires Vécues". Ça représentait un serpent boa qui avalait un fauve. Voilà la copie du dessin.
+        title: 'Chapitre I : Un enfant de Nkoabang',
+        content: `La ville de Yaoundé s’éveillait lentement sous les premiers rayons du soleil. Dans le quartier
+Nkaobang, les rues de terre rouge retrouvaient leur agitation habituelle. Les mototaxis traversaient
+les ruelles à toute vitesse, les commerçantes installaient leurs étals, tandis que les enfants, cartable
+au dos, se dirigeaient vers leurs écoles.
 
-On disait dans le livre : "Les serpents boas avalent leur proie tout entière, sans la mâcher. Ensuite ils ne peuvent plus bouger et ils dorment pendant les six mois de leur digestion."
+Au milieu de cette foule vivait un adolescent nommé Damien. Agé de douze ans, il habitait avec ces
+parents et ses deux jeunes frères dans une petite maison construite en planches et recouvert de
+tôles rouillés. Pendant la saison des pluies, l’eau s’infiltrait par le toit. La nuit, toute la famille plaçait
+des bassines dans différentes pièces pour recueillir les gouttes qui tombaient au plafond.
 
-J'ai alors beaucoup réfléchi sur les aventures de la jungle et, à mon tour, j'ai réussi, avec un crayon de couleur, à tracer mon premier dessin. Mon dessin numéro 1. Il était comme ça :
+Son père, autrefois ouvrier sur des chantiers, ne trouvait plus des emplois occasionnels. Sa mère
+gagnait difficilement sa vie en vendant quelques légumes, des arachides grillées et des beignets. Les
+revenus de la famille ne suffisaient presque jamais à couvrir les dépenses essentielles.
 
-J'ai montré mon chef-d'œuvre aux grandes personnes et je leur ai demandé si mon dessin leur faisait peur.
-Elles m'ont répondu : "Pourquoi un chapeau ferait-il peur ?"
+Malgré cette pauvreté, Damien nourrissait un rêve immense. Il voulait réussir, non pour devenir
+célèbre, mais pour offrir une vie meilleure a ceux qu’il aimait. Il répétait sauvent en silence :
 
-Mon dessin ne représentait pas un chapeau. Il représentait un serpent boa qui digérait un éléphant. J'ai alors dessiné l'intérieur du serpent boa, afin que les grandes personnes puissent comprendre. Elles ont toujours besoin d'explications. Mon dessin numéro 2 était comme ça...
+« Un jour, mes parents ne manqueront plus de rien.
+Chaque matin, il parcourait plusieurs kilomètres à pied pour rejoindre son collège. Ses chaussures
+étaient usées, son uniforme avait été recousu à plusieurs reprises, mais il gardait toujours la tête
+haute.
 
-Les grandes personnes m'ont conseillé de laisser de côté les dessins de serpents boas ouverts ou fermés, et de m'intéresser plutôt à la géographie, à l'histoire, au calcul et à la grammaire. C'est ainsi que j'ai abandonné, à l'âge de six ans, une magnifique carrière de peintre. J'avais été découragé par l'insuccès de mon dessin numéro 1 et de mon dessin numéro 2. Les grandes personnes ne comprennent jamais rien toutes seules, et c'est fatigant, pour les enfants, de toujours et toujours leur donner des explications.`,
+A la fin des cours, pendant que ses camarades rentraient joue au football, Damien se rendait au
+marché de Nkoabang. Là, il aidait les commerçantes à transporter les sacs de riz, des cartons d’huile
+ou des régimes de plantains et bananes. En échange, il recevait quelques centaines de francs CFA
+Les jours ou le travail manquait, il ramassait des bouteilles en plastiques et de vieux morceaux de
+ferrailles qu’il revendait à des recycleurs.
+
+ Chaque pièce gagnée était précieusement conservée dans
+une petites boite métallique cachée sous son lit. Cet argent servait à acheter des cahiers, des stylos
+et parfois à payer une partie de ses frais de scolarité.
+
+Un soir, alors qu’il rentrait chez lui, épuisé, il aperçut sa mère assise devant la maison. Elle essuyait
+discrètement une larme.
+
+- Maman qu’est qui ne va pas ?
+  Demanda Damien.
+  Elle tenta de sourire.
+
+- Ce n’est rien, mon fils. Je me demande seulement comment nous allons payer le loyer ce mois-ci.
+
+
+Ces mots restèrent gravés dans son cœur.
+
+Cette nuit-là, Damien ne dormit presque pas. Il regardait les étoiles à travers les ouvertures
+du toit et pris une décision qui allait changer toute son existence.
+
+Il se promit de ne jamais abandonner ses études, quelles que soient les difficultés. Il
+travaillerait davantage, apprendrait sans relâche et construirait son avenir pierre après
+pierre.
+
+
+`,
         durationMin: 5
       },
       {
         id: 'lpp-ch2',
-        title: 'Chapitre II : S\'il vous plaît... dessine-moi un mouton !',
-        content: `J'ai ainsi vécu seul, sans personne avec qui parler véritablement, jusqu'à une panne dans le désert du Sahara, il y a six ans. Quelque chose s'était cassé dans mon moteur. Et comme je n'avais avec moi ni mécanicien, ni passagers, je me préparai à tenter, tout seul, une réparation difficile. C'était pour moi une question de vie ou de mort. J'avais à peine de l'eau à boire pour huit jours.
+        title: 'Chapitre II : Les blessures de la pauvreté',
+        content: `Le lendemain de sa promesse, Damien se réveilla avant l’aube. Un léger vent soufflait à travers les
+fissures des murs de la maison familiales. Il enfila son uniforme soigneusement lavé à la veille par sa
+mère et prit la route de l’école.
 
-Le premier soir je me suis donc endormi sur le sable à mille milles de toute terre habitée. J'étais bien plus isolé qu'un naufragé sur un radeau au milieu de l'océan. Alors vous imaginez ma surprise, au lever du jour, quand une drôle de petite voix m'a réveillé. Elle disait :
-— S'il vous plaît... dessine-moi un mouton !
-— Hein !
-— Dessine-moi un mouton...
+Son ventre était vide. A la maison, il ne restait qu’un peu de farine de maïs pour préparer le repas du
+soir. Malgré la faim qui le tenaillait, il gardait le sourire. Au collège, les cours se succédèrent Damien
+était un élève brillant. Les mathématiques, l’histoire, sciences le passionnaient. Ses enseignants
+admiraient son sérieux, mais ignoraient les sacrifices qu’il consentait chaque jour pour rester sur les
+bancs de l’école.
 
-Je me suis dressé sur mes pieds comme si j'avais été frappé par la foudre. J'ai bien frotté mes yeux. J'ai bien regardé. Et j'ai vu un petit bonhomme tout à fait extraordinaire qui me considérait avec gravité. Voilà le meilleur portrait que, plus tard, j'ai réussi à faire de lui.
+A la récréation, plusieurs élèves achetèrent des sandwichs et des boissons. Damien s’assit
+discrètement sous un manguier avec des cahier entre les mains.
 
-Je regardai donc cette apparition avec des yeux tout ronds d'étonnement. N'oubliez pas que je me trouvais à mille milles de toute région habitée. Or mon petit bonhomme ne me semblait ni égaré, ni mort de fatigue, ni mort de faim, ni mort de soif, ni mort de peur. Il n'avait en rien l'apparence d'un enfant perdu au milieu du désert, à mille milles de toute région habitée. Quand je réussis enfin à parler, je lui dis :
-— Mais... qu'est-ce que tu fais là ?
-Et il me répéta alors, tout doucement, comme une chose très sérieuse :
-— S'il vous plaît... dessine-moi un mouton...`,
+- Pourquoi tu ne manges jamais ?
+
+Demanda son camarade Boris.
+
+Damien sourit.
+
+- Je n’ai pas très faim.
+
+Il préféra cacher la vérité plutôt que d’avouer qu’il n’avait pas un seul franc dans ces poches.
+
+Quelque semaine plus tard, un événement bouleversa sa vie. Le directeur annonça que tous les
+élèves devaient payer la deuxième tranche des frais de scolarité avant la fin du mois. En rentrant à la
+maison, Damien trouva ses parents plongés dans un profond silence.
+
+- Papa… il faut payer les frais de scolarité.
+
+Son père baissa les yeux.
+
+- Mon fils…J’ai cherché du travail toute la semaine. Personne ne m’a embauché.
+
+Sa mère posa doucement une main sur son épaule.
+-
+Pardonne -nous. Nous faisons tout notre possible.
+Ces paroles furent comme un coup de poignard. Damien comprit que ses parents souffraient autant
+que lui.
+
+Le lendemain, il décida de travailler davantage. Après les cours, il passait plusieurs heures au marché
+Nkoabang à transporter des lourds sacs de riz, des cartons d’huile et savons des cageots de tomates.
+Ses mains étaient couvertes d’ampoules. Son dos lui faisait souffrit. Pourtant, il ne se plaignait
+jamais. Un vieux commerçant, nommé papa Martin, observait sauvent ce jeune garçon courageux
+
+-
+Damien, pourquoi travailles-tu autant à ton âge ?
+Le jeune homme répondit avec calme :
+-
+
+Parce que je veux terminer mes études. Un jour, je deviendrais grand entrepreneur. Je
+créerai des entreprises qui donnerons du travail a beaucoup de personnes.
+Le vieil homme esquissa un sourire.
+
+-
+Les grands hommes commencent sauvent par de petits sacrifices. Ne perd jamais la
+détermination. Ces mots donnèrent à Damien une force nouvelle.
+
+
+Pendant plusieurs semaines, il économisa chaque franc gagné. Lorsqu’il réunit en fin la somme
+nécessaire, il se rendit au collège pour payer ses frais de scolarité. En sortant du bureau du
+comptable, il leva les yeux vers le ciel.
+
+
+Pour la première fois, il comprit que chaque obstacle franchi le rapprochait se son rêve. Il ignorait
+encore que les difficultés ne faisaient que commerce. Mais il avait désormais une certitude : tant
+qu’il garderait la foi, le courage et la discipline, rien ne pourrait l’empêcher de réussir.
+
+
+`,
         durationMin: 8
-      }
-    ]
-  },
-  {
-    id: 'l-art-de-la-guerre',
-    title: 'L\'Art de la Guerre',
-    author: 'Sun Tzu',
-    genre: 'Philosophie & Stratégie',
-    description: 'Le premier traité de stratégie militaire écrit au monde. Une leçon intemporelle de résolution de conflits s\'appliquant à tous les aspects de la vie quotidienne.',
-    coverColor: 'from-amber-950 via-stone-905 to-neutral-950',
-    coverDesignType: 'classic',
-    createdAt: '2026-02-10T10:00:00Z',
-    tags: ['Stratégie', 'Philosophie', 'Sagesse', 'Histoire'],
-    pagesCount: 140,
-    chapters: [
-      {
-        id: 'adg-ch1',
-        title: 'Chapitre I : Des Plans et Estimations',
-        content: `Sun Tzu dit : La guerre est d'une importance vitale pour l’État. C'est le domaine de la vie et de la mort, la voie vers la survie ou la ruine. Par conséquent, il est indispensable de l’étudier en détail.
-
-Pour estimer ses chances de succès, il faut analyser la situation selon cinq facteurs fondamentaux :
-1. La Loi Morale (faire en sorte que le peuple soit en parfaite harmonie avec son souverain).
-2. Le Ciel (les forces de la nature, le climat, le chaud et le froid, le cycle des saisons).
-3. La Terre (les distances, le relief du terrain, la sécurité et le danger).
-4. Le Commandant (les vertus de sagesse, de sincérité, de bienveillance, de courage et de rigueur).
-5. La Discipline (l'organisation des troupes, la logistique et l'encadrement).
-
-Toute campagne militaire repose sur l'art de l’illusion. Quand nous sommes capables d'attaquer, nous devons paraître incapables de le faire ; quand nous utilisons nos forces, nous devons sembler inactifs ; quand nous sommes proches de l’ennemi, nous devons lui faire croire que nous sommes loin.`,
-        durationMin: 6
       },
-      {
-        id: 'adg-ch2',
-        title: 'Chapitre II : De la Conduite de l\'Action',
-        content: `Dans les opérations militaires, une fois la décision prise d'engager le combat, l'objectif absolu doit être une victoire rapide. Si la guerre se prolonge, les ressources de l’État s'épuisent, le tranchant des armes s’émousse et le courage des hommes s’effrite.
 
-Aucun pays n'a jamais tiré profit d’une guerre prolongée. Un général habile sait vivre sur le territoire ennemi et s’emparer des provisions de l’adversaire. Nourrir ses armées aux dépens de l'ennemi vaut vingt fois plus cher que de transporter ses propres rations depuis de longues distances.
+       {
+        id: 'lpp-ch3',
+        title: 'Chapitre II : Les leçons du marché',
+        content: `Le marché de Nkoabang s’éveillait bien avant le lever du soleil. Dès cinq heure du matin, les premiers
+commerçants installaient leurs marchandises. Les cris des vendeurs se mêlaient au bruit des camions
+qui déchargeaient des sacs de riz des cartons de savons, des cageots de tomates et des régimes de
+bananes plantains.
 
-Traitez vos prisonniers de guerre avec humanité et respect, car l'intérêt principal est de vaincre l'ennemi et de s'enrichir par sa propre défaite, sans détruire votre propre intégrité.`,
-        durationMin: 7
+Pour Damien, ce marché était devenu une seconde école. Chaque après-midi, après les cours, il
+déposait son cartable à la maison et repartait aussitôt vers le marché. Il y retrouvait les porteurs, les
+commerçants et les revendeurs de ferraille qui le connaissaient déjà.
+
+- Damien, viens m’aider à transporter ces sacs ! criait sauvent une commerçante. Sans hésiter,
+il chargeait les lourds sacs sur son dos et parcourait plusieurs mètres sous un soleil brûlant. A
+la fin de journée, il gagnait parfois mille francs CFA, parfois deux mille lorsqu’il avait de la
+chance.
+Un samedi matin, alors qu’il aidait un vieil homme à ranger son magasin, celui-ci l’observa
+avec attention.
+
+- Tu t’appelles comment, mon garçon ?
+- Damien, monsieur.
+- Et pourquoi travailles-tu autant ?
+ Damien hésita quelques secondes avant de répondre.
+- Je veux terminer mes études. Un jour, je veux devenir un grand entrepreneur pour sortie ma
+famille de la pauvreté.
+Le vieil homme sourit.
+- Je m’appelle Martin. Ici, tout le monde m’appelle papa Martin. Ecoute-moi bien, Damien.
+Beaucoup de jeunes cherchent seulement à gagner de l’argent. Très peu cherchent à
+comprendre comment le créer.
+
+Ces paroles intriguèrent Damien.
+A partir de ce jour, chaque fois qu’il terminait son travail, il restait quelques minutes auprès
+de papa martin, le vieil homme lui expliquait comment réinvestir les bénéfices au lieu de tout
+dépenser et surtout, comment gagner la confiance des clients.
+
+- L’argent vient et repart, disait-il. Mais une bonne réputation peut nourrir un homme toute sa
+vie.
+
+Ces leçons marquèrent profondément Damien. Quelques jours plus tard, papa Martin lui
+remit un vieux cahier.
+
+- Tiens. A partir d’aujourd’hui, note tout ce que tu gagnes et tout ce que tu dépense. Un
+entrepreneur doit connaitre la valeur de chaque franc.
+Damien suivit ce conseil avec rigueur. Chaque soir, à la lumière d’une lampe ç pétrole, il
+inscrivait soigneusement ses recettes et ses dépenses. Peu à peu, il apprit à économiser.
+Pendant ce temps, les résultats scolaires continuaient de s’améliorer. Ses professeurs
+remarquaient son sérieux et son assiduité. Un après-midi son professeur d’économie
+l’interrogea devant toute la classe.
+- Damien, que veux-tu faire plus tard ?
+Le jeune garçon e leva lentement.
+
+- Monsieur, je veux créer des entreprises qui donneront du travail aux jeunes. Je veux prouver
+que même un enfant pauvre peut réussir honnêtement.
+Toute la classe éclata de rire.
+
+- Toi ? Un chef d’entreprise ? Lança un élève en se moquant.
+
+Damien ne répondit pas. Il se rassit calmement.
+Au fond de lui, il se fit une promesse : un jour, ceux qui riaient de lui verraient que les rêves
+ne sont pas faits pour être abandonnés, mais pour être réalisés.
+Les années passaient, et le jeune garçon devenait un homme. Sans le savoir, la vie s’apprêtait
+à lui offrit une rencontre qui allait bouleverser son destin.
+
+`,
+        durationMin: 8
       }
     ]
   },
@@ -122,5 +250,8 @@ N'attendez plus ! Passez en mode Éditeur via l'onglet en haut de l'écran, entr
         durationMin: 5
       }
     ]
-  }
+  },
+
+
+  
 ];
